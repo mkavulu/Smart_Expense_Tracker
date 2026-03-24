c3c348899107151df8a4ea7307ff67885626bca8
mkavulu <mutukukavulu2000@gmail.com>
2026-03-24 19:10:19 +0300

    made changes

--- Smart_Expense_Tracker/settings.py
+++ Smart_Expense_Tracker/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
 #ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+ALLOWED_HOST = ['smart-expense-tracker-ba8v.onrender.com']
 
 
 
